7ed2d1f3df252d89ad54a56a3deb96da8653a5f7
superwibr <superwibr@gmail.com>
2023-02-13 20:30:58 -0500

    fuck it all

--- src/local.js
+++
@@ -1,9 +0,0 @@
-import { Game } from "./core.js";
-import { piecesDir } from "./pieces.js"
-
-var game = new Game([2,2])
-
-game.createPiece("pawn", piecesDir.pawn)
-
-// expose
-window.Piece = Piece;
--- src/pieces.js
+++
@@ -1,5 +0,0 @@
-export default piecesDir = {
-	pawnf: {
-		
-	}
-}
